324aa472e945fe183312c25432e5f0646e9ebb16
Roman Thiele <roman.thiele@stustanet.de>
2011-10-29 14:56:36 +0200

    Almost done

--- printer-gui.py
+++ printer-gui.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
-# vim: set fileencoding=utf-8 st=4 sts=4 et:
+# vim: set fileencoding=utf-8 ts=4 sts=4 et:
 
-PRICE_CMD = '/home/roman/Git/ssn-printer-gui/price-dummy'
-PRINT_CMD = '/home/roman/Git/ssn-printer-gui/print-dummy'
+PRICE_CMD = '/usr/local/bin/printer_pricecalc.py'
+PRINT_CMD = '/home/a004647a/ssn-printer-gui/print-dummy'
+
+LOGO = '/usr/local/share/ssn/StuStaNet_Logo.svg'
 
 import subprocess
 
@@ -14,6 +16,10 @@ import gobject
 class PrinterGui:
     # file to print
     file = None
+
+    # duplex and blackwhite settings
+    duplex = None
+    blackwhite = False
 
     # current state
     state = None
@@ -32,15 +38,48 @@ class PrinterGui:
         # create window
         self.win = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.win.set_title("StuStaNet Druckservice")
+        self.win.set_border_width(5)
         
         # create central vbox
         self.vbox = gtk.VBox(homogeneous=False, spacing=0)
+
+        # logo
+        frm = gtk.Frame()
+        logo = gtk.Image()
+        logo.set_from_file(LOGO)
+        logo.show()
+        frm.add(logo)
+        frm.show()
+        self.vbox.pack_start(frm)
 
         # ensure proper end of application
         self.win.connect('delete_event', lambda w, e: gtk.main_quit())
         self.win.connect('destroy', self.quit)
 
+
+        # uploaded files
+        frm = gtk.Frame(label=" Hochgeladene Dateien ")
+        self.upl_hbox = gtk.HBox(homogeneous=False, spacing=0)
+        self.upl_hbox.set_border_width(5)
+        lbl = gtk.Label("PIN: ")
+        lbl.show()
+        self.upl_hbox.pack_start(lbl, fill=False, expand=False)
+        self.pin_input = gtk.Entry()
+        self.pin_input.show()
+        self.upl_hbox.pack_start(self.pin_input)
+        self.upl_button = gtk.Button("Laden")
+        self.upl_button.connect('clicked', self.load_uploaded, None)
+        self.upl_button.show()
+        self.upl_hbox.pack_start(self.upl_button, padding=5, fill=False, expand=False)
+        self.upl_hbox.show()
+        frm.add(self.upl_hbox)
+        frm.show()
+        self.vbox.pack_start(frm)
+
         # file chooser button
+        frm = gtk.Frame(label=' Dateien von USB-Stick oder Festplatte ')
+        hbox = gtk.HBox(homogeneous=False, spacing=0)
+        hbox.set_border_width(5)
         self.open_filter = gtk.FileFilter()
         self.open_filter.add_pattern('*.pdf')
         self.open_filter.set_name('PDF Dateien')
@@ -57,20 +96,45 @@ class PrinterGui:
         self.open_button.set_width_chars(30)
         self.open_button.connect('file-set', self.file_selected, None)
         self.open_button.show()
-        self.vbox.pack_start(self.open_button)
+        lbl = gtk.Label('Datei öffnen: ')
+        lbl.show()
+        hbox.pack_start(lbl, expand=False, fill=False)
+        hbox.pack_start(self.open_button)
+        hbox.show()
+        frm.add(hbox)
+        frm.show()
+        self.vbox.pack_start(frm)
 
         # duplex and b/w checkboxes
+        frm = gtk.Frame(" Druckeinstellungen ")
+        vbox = gtk.VBox(homogeneous=False, spacing=0)
         self.bw_checkbox = gtk.CheckButton(label="Schwarz / Weiss")
-        self.duplex_checkbox = gtk.CheckButton(label="Duplex")
         self.bw_checkbox.show()
-        self.duplex_checkbox.show()
-        self.vbox.pack_start(self.bw_checkbox)
-        self.vbox.pack_start(self.duplex_checkbox)
+        self.simplex_radio = gtk.RadioButton(None, "Einseitig")
+        self.simplex_radio.show()
+        self.simplex_radio.connect('toggled', self.checkbox_changed, None)
+        #self.simplex_radio.set_active(True)
+        self.long_edge_radio = gtk.RadioButton(self.simplex_radio, "Doppelseitig (lange Seite)")
+        self.long_edge_radio.show()
+        self.long_edge_radio.connect('toggled', self.checkbox_changed, None)
+        self.short_edge_radio = gtk.RadioButton(self.simplex_radio, "Doppelseitig (kurze Seite)")
+        self.short_edge_radio.show()
+        self.short_edge_radio.connect('toggled', self.checkbox_changed, None)
+        self.bw_checkbox.connect('toggled', self.checkbox_changed, None)
+        vbox.pack_start(self.simplex_radio)
+        vbox.pack_start(self.long_edge_radio)
+        vbox.pack_start(self.short_edge_radio)
+        vbox.pack_start(self.bw_checkbox)
+        vbox.show()
+        frm.add(vbox)
+        frm.show()
+        self.vbox.pack_start(frm)
 
         # button
         self.action_button = gtk.Button(
                 label="Berechnen"
             )
+        self.action_button.child.set_use_markup(True)
         self.action_button.connect('clicked', self.do_action, None)
         self.action_button.show()
         self.vbox.pack_start(self.action_button)
@@ -87,6 +151,22 @@ class PrinterGui:
     def file_selected(self, btn, data):
         self.file = btn.get_filename()
         if self.file:
+            self.state = 'count'
+        self.apply_state()
+
+    def load_uploaded(self, btn, data):
+        print "GET uploaded file"
+
+    def checkbox_changed(self, btn, data):
+        if self.simplex_radio.get_active():
+            self.duplex = None
+        elif self.long_edge_radio.get_active():
+            self.duplex = 'long'
+        elif self.short_edge_radio.get_active():
+            self.duplex = 'short'
+
+        self.blackwhite = self.bw_checkbox.get_active()
+        if self.state:
             self.state = 'count'
         self.apply_state()
 
@@ -112,24 +192,84 @@ class PrinterGui:
                 self.price = float(self.count_proc.stdout.read())
                 self.apply_state()
                 return False
+        else:
+            return False
 
         return True # call me again
+
+    def get_billing_string(self):
+        return (self.blackwhite and "black" or "color") + "_" + (self.duplex and "duplex" or "simplex")
 
     def do_count(self):
         gobject.timeout_add_seconds(1, self.timer_event, None)
         self.price_buffer = ""
+        self.kill_subproc()
         self.count_proc = subprocess.Popen(
-                (PRICE_CMD, self.file),
+                (
+                    PRICE_CMD,
+                    'pricecalc',
+                    self.get_billing_string(),
+                    self.file,
+                    'stdout'
+                ),
                 bufsize=(1024*16),
                 stdout=subprocess.PIPE
             )
+
+    def kill_subproc(self):
+        try:
+            if self.count_proc:
+                self.count_proc.terminate()
+                self.count_proc = None
+        except:
+            pass
     
     def do_print(self):
-        subprocess.Popen((PRINT_CMD, self.file))
+        duplex = 'None'
+        if self.duplex == 'long':
+            duplex = 'DuplexNoTumble'
+        elif self.duplex == 'short':
+            duplex = 'DuplexTumble'
+
+        color = 'CMYK'
+        if self.blackwhite:
+            color = 'Gray'
+
+        subprocess.Popen(
+                (
+                    '/usr/bin/lp',
+                    '-o', "Duplex=%s" % duplex,
+                    '-o', "ColorModel=%s" % color,
+                    '-o', "job-billing=%s" % self.get_billing_string(),
+                    self.file
+                )
+            )
+        self.state = None
+        self.open_button.unselect_all()
+        self.file = None
+        self.duplex = None
+        self.blackwhite = False
+        self.apply_state()
 
     def apply_state(self):
         """Modify widgets based on self.state"""
         print("DEBUG: state = %s" % repr(self.state))
+
+        if self.state != 'progress':
+            self.kill_subproc()
+
+        # set filename
+        if self.file:
+            self.open_button.set_filename(self.file)
+        if self.duplex == 'long':
+            self.long_edge_radio.set_active(True)
+        elif self.duplex == 'short':
+            self.short_edge_radio.set_active(True)
+        else:
+            self.simplex_radio.set_active(True)
+  
+        self.bw_checkbox.set_active(self.blackwhite)
+
         # set state of button
         if self.state == 'count' or self.state == 'print':
             self.action_button.set_sensitive(True)
@@ -147,16 +287,13 @@ class PrinterGui:
                 self.action_button.set_label('Berechnung läuft...')
                 self.progress_state = True
         elif self.state == 'print':
-            self.action_button.set_label("Drucken für <b>%0.2f EUR</b>" % self.price)
+            self.action_button.child.set_markup("Drucken für <b>%0.2f EUR</b>" % self.price)
         else:
             raise "State engine damaged, invalid state = %s" % repr(self.state)
 
-        if self.state == None:
-            #self.open_button.set_filename("")
-            self.open_button.unselect_all()
-
     def quit(self, *args, **kwargs):
         """Quit the mainloop."""
+        self.kill_subproc()
         gtk.main_quit()
 
     def main(self):
